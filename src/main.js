36bc967de5e1b21d3a8afb8240e80acb3579d05e
krushndayshmookh <krushndayshmookh@gmail.com>
2020-04-10 13:36:26 +0530

    import styles

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
+import 'materialize-css/sass/materialize.scss'
+import '@/assets/css/dayshmookh.layout.css'
+import '@/assets/style.scss'
+Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
