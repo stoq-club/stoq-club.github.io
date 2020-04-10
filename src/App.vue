c839a71f502aac3bc4d9bea384bc71aa37d1340c
krushndayshmookh <krushndayshmookh@gmail.com>
2020-04-10 13:36:15 +0530

    create header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  #app
+    Header
+    main
+      section.section.margined-top-cozy
+        .container
+          .row
+            .col.s12.m4
+              .card-panel.center.feature-card
+                img(src="/images/yuna.jpg")
+                h6 Lower minimums
+                p easier ascces to adjnfsd fnjsdfjknsf kjfsnj dajkdjna dadjasdh dasdas
+            .col.s12.m4
+              .card-panel.center.feature-card.offset-top
+                img(src="/images/yuna.jpg")
+                h6 Lower minimums
+                p easier ascces to adjnfsd fnjsdfjknsf kjfsnj dajkdjna dadjasdh dasdas
+            .col.s12.m4
+              .card-panel.center.feature-card
+                img(src="/images/yuna.jpg")
+                h6 Lower minimums
+                p easier ascces to adjnfsd fnjsdfjknsf kjfsnj dajkdjna dadjasdh dasdas
+      section.section.green.inset-top
+        .container
+          .row
+
+
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Header from './components/Header.vue'
+import M from 'materialize-css'
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  name: 'App',
+  components: { Header },
+  mounted() {
+    M.AutoInit()
   }
-};
+}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.inset-top {
+  margin-top: -256px;
+}
+
+.feature-card {
+  border-radius: 8px !important;
+  -webkit-box-shadow: 0px 4px 16px -8px rgba(75, 75, 75, 0.75);
+  -moz-box-shadow: 0px 4px 16px -8px rgba(75, 75, 75, 0.75);
+  box-shadow: 0px 4px 16px -8px rgba(75, 75, 75, 0.75);
+
+  p {
+    font-family: 'Futura Light';
+  }
+
+  &.offset-top {
+    margin-top: 64px;
+  }
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  nav.transparent.z-depth-0
+    .container.nav-wrapper
+      a.brand-logo.green-text(href="/") Stoq
+
+      ul.right.hide-on-small
+        li
+          a.green-text.strong(href="/about") About
+        
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
+
+<style scoped></style>
