4ea83dea7b2f2e0660a2c392684be7be3d659ca6
krushndayshmookh <krushndayshmookh@gmail.com>
2020-04-12 16:03:21 +0530

    cleanup

--- src/components/FeatureCard.vue
+++ src/components/FeatureCard.vue
@@ -2,7 +2,7 @@
     .card-panel.center.feature-card(:class="{'offset-top':offsetTop}")
         img(:src="feature.img")
         h6 {{ feature.title }}
-        p {{ feature.description }}
+        p.light {{ feature.description }}
 </template>
 
 <script>
@@ -12,17 +12,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.feature-card {
-  border-radius: 8px !important;
-  -webkit-box-shadow: 0px 4px 16px -8px rgba(75, 75, 75, 0.75);
-  -moz-box-shadow: 0px 4px 16px -8px rgba(75, 75, 75, 0.75);
-  box-shadow: 0px 4px 16px -8px rgba(75, 75, 75, 0.75);
-
-  p {
-    font-family: 'Futura Light';
-  }
-}
-
 @media only screen and (min-width: 601px) {
   .offset-top {
     margin-top: 64px;
