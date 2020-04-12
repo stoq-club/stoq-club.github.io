3dad67f819f84c2e20ab2f245aab190b375f3d62
krushndayshmookh <krushndayshmookh@gmail.com>
2020-04-12 16:03:48 +0530

    design blogs page

---
+++ src/views/Blog.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  .blog
+    section.section
+      .container.center.margined-top-comfortable.margined-bottom-comfortable
+        Nugget(color="green")
+        h5.center Keep upto date with our blog posts
+        .margined-top-comfortable
+          .input-field
+            input.browser-default(type="text" placeholder="Find something...")
+            .btn.btn-flat.green.white-text Search
+
+    section.section.margined-top-comfortable.margined-bottom-comfortable
+      .container
+        .row
+          .col.s6.m4
+            .card.hoverable
+              .card-image
+                img(src="/images/office.jpg", alt="alt")
+              .card-content
+                span.card-title.green-text Title of post
+                p.light Some small description or exerpt
+
+          .col.s6.m4
+            .card.hoverable
+              .card-image
+                img(src="/images/office.jpg", alt="alt")
+              .card-content
+                span.card-title.green-text Title of post
+                p.light Some small description or exerpt
+
+          .col.s6.m4
+            .card.hoverable
+              .card-image
+                img(src="/images/office.jpg", alt="alt")
+              .card-content
+                span.card-title.green-text Title of post
+                p.light Some small description or exerpt
+
+</template>
+
+<style scoped lang="scss">
+.input-field {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
+
+<script>
+import Nugget from '@/components/Nugget'
+export default {
+  components: {
+    Nugget
+  }
+}
+</script>
