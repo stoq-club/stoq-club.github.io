5872920eeae53f6ddf4f7b5f314a9577505107cb
krushndayshmookh <krushndayshmookh@gmail.com>
2020-04-13 22:48:05 +0530

    fix spacing

--- src/components/FeatureCard.vue
+++ src/components/FeatureCard.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-    .card-panel.center.feature-card(:class="{'offset-top':offsetTop}")
+    .card-panel.center.feature-card.margined-compact(:class="{'offset-top':offsetTop}")
         img(:src="feature.img")
         h6 {{ feature.title }}
         p.light {{ feature.description }}
